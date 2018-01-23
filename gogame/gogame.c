7bad903dc77b37054cb77315beaa4c455a3e5d84
Laurent Demailly <ldemailly@gmail.com>
2018-01-22 22:10:33 -0800

    Experiment in timing in C

---
+++ gogame/gogame.c
@@ -0,0 +1,64 @@
+// (C) 2017-2018 All Rights Reserved Laurent Demailly.
+
+#include <stdio.h>
+#include <sys/resource.h>
+#include <time.h>
+
+clock_t t;
+
+void startTimer() {
+    t = clock();
+}
+
+clock_t endTimer() {
+    clock_t n = clock();
+    clock_t elapsed = n - t;
+    t = n;
+    return elapsed;
+}
+
+struct rusage usage;
+
+void startUsage() {
+    if (getrusage(RUSAGE_SELF, &usage)) {
+        perror("getrusage");
+    }
+}
+
+void endUsage(struct rusage *delta) {
+    struct rusage tempu;
+    if (getrusage(RUSAGE_SELF, &tempu)) {
+        perror("getrusage");
+    }
+    long *ptrNew = (long *)&tempu;
+    long *ptrPrv = (long *)&usage;
+    long *ptrRes = (long *)delta;
+    // substract all fields as long
+    int i = 0;
+    for (int i = 0;
+         ((char *)ptrNew) <= ((char *)&tempu) + sizeof(tempu) - sizeof(long);
+         i++) {
+        long diff = (*ptrNew) - (*ptrPrv);
+        printf("%d: %ld\n", i, diff);
+        *(ptrRes++) = diff;
+        *(ptrPrv++) = *(ptrNew++);
+    }
+}
+
+void printTimer(char *msg, clock_t v) {
+    printf("%s: %lu clicks (%f us)\n", msg, v,
+           1000000. * ((double)v) / CLOCKS_PER_SEC);
+}
+
+int main() {
+    printf("timeval sz = %lu - %lu\n", sizeof(struct timeval), sizeof(long));
+    clock_t v;
+    struct rusage delta;
+    startTimer();
+    v = endTimer();
+    printTimer("no op", v);
+    startUsage();
+    endUsage(&delta);
+    printf("end - clocks per sec %d\n", CLOCKS_PER_SEC);
+    return 0;
+}
